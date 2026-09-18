00f09a8f9d7acbe19b5b2f6089b9dbd3bdc5f0ee
ahmadnasarsalah-cmyk <ahmadnasarsalah@gmail.com>
2026-09-18 14:10:28 +0300

    Update data.js

--- Platform_Project/data.js
+++ Platform_Project/data.js
@@ -28,7 +28,50 @@ const platformData = [
             { title: "حل تمارين مقارنة الأعداد النسبية", youtubeId: "n_iqEg2Gz84" },
             { title: "جمع الأعداد النسبية وخواصها", youtubeId: "eoy6xlisJog" },
             { title: "حل تمارين جمع الأعداد النسبية وخواصها", youtubeId: "_ggTUe4H21Y" },
-            { title: "طرح الأعداد النسبية", youtubeId: "En4Czy50kxE" }
+            { title: "طرح الأعداد النسبية", youtubeId: "En4Czy50kxE" },
+            { title: "حل تمارين طرح الأعداد النسبية", youtubeId: "avRA5R6WtNQ" },
+            { title: "ضرب الأعداد النسبية", youtubeId: "0bYHoo85-AU" },
+            { title: "حل تمارين ضرب الأعداد النسبية", youtubeId: "77Z-L9sEXcw" },
+            { title: "قسمة الأعداد النسبية", youtubeId: "usRj9QUZvS4" },
+            { title: "تكملة حل تمارين قسمة الأعداد النسبية", youtubeId: "PHUe-H__cyI" },
+            { title: "الجذر التربيعي للعدد النسبي مربع كامل", youtubeId: "BRKBvrKmG2s" },
+            { title: "الجذر التربيعي للعدد النسبي ليس مربع", youtubeId: "QaNb0YIk2ck" },
+            { title: "الجذر التكعيبي للأعداد النسبية", youtubeId: "c6nJg39fWDc" }
+        ]
+    },
+    {
+        unitName: "الوحدة الثالثة",
+        videos: [
+            { title: "مراجعة", youtubeId: "NOumXeKmWZE" },
+            { title: "ضرب مقدار جبري في حد جبري", youtubeId: "FXeLnAFb3eE" },
+            { title: "تمارين ضرب مقدار جبري في حد جبري", youtubeId: "wKZq6lgWeCs" },
+            { title: "قسمة مقدار جبري على حد جبري", youtubeId: "vaLIQ5xrbA8" },
+            { title: "حل تمارين قسمة مقدار جبري على حد جبري", youtubeId: "T62NCYCg3bw" },
+            { title: "ضرب المقادير الجبرية", youtubeId: "9nR5Ebz6FUs" },
+            { title: "تكملة حل تمارين ضرب المقادير الجبرية", youtubeId: "Mz-ncFnoQ4Y" },
+            { title: "قسمة المقادير الجبرية", youtubeId: "W8pASmb6ezw" },
+            { title: "تمارين قسمة المقادير الجبرية", youtubeId: "5xHGBDlya8k" },
+            { title: "التحليل باستخراج العامل المشترك الأكبر", youtubeId: "ING3CI8_eD8" },
+            { title: "تابع التحليل باستخراج العامل المشترك الأكبر", youtubeId: "xE0WylvTLss" },
+            { title: "تحليل الفرق بين مربعين", youtubeId: "QRPREFO7PUo" },
+            { title: "تمارين ومسائل عامة", youtubeId: "QRPREFO7PUo" },
+            { title: "تكملة حل تمارين ومسائل عامة", youtubeId: "NJN4cspblrQ" }
+        ]
+    },
+    {
+        unitName: "الوحدة الرابعة",
+        videos: [
+            { title: "المعادلات من الدرجة الأولى في متغير واحد", youtubeId: "SGXMasm5xpw" },
+            { title: "حل تمارين معادلات من الدرجة الأولى في متغير واحد", youtubeId: "SGXMasm5xpw" },
+            { title: "تكملة حل تمارين معادلات من الدرجة الأولى في متغير واحد", youtubeId: "bnUFBjoxHEw" },
+            { title: "متراجحة من الدرجة الأولى في متغير واحد", youtubeId: "byCaXaGaUeU" },
+            { title: "تابع شرح متراجحة من الدرجة الأولى في متغير واحد", youtubeId: "2Ubh9UEqcow" },
+            { title: "حل تمارين المتراجحة من الدرجة الأولى في متغير واحد", youtubeId: "rji8_OB14GU" },
+            { title: "معادلة من الدرجة الثانية في متغير واحد", youtubeId: "0ynIqZ_1Kws" },
+            { title: "حل تمارين معادلة من الدرجة الثانية في متغير واحد", youtubeId: "hqWNoSbSVkc" },
+            { title: "مسائل تطبيقية", youtubeId: "68Ji4Yb9IIw" },
+            { title: "حل تمارين مسائل تطبيقية", youtubeId: "bywf9CAojd8" },
+            { title: "تمارين ومسائل عامة للوحدة", youtubeId: "Xm9FAaDkxNs" }
         ]
     }
 ];
